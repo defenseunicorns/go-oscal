feb9892849ee2f58b04fb6ecdcac6d74b4daedcb
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2023-02-01 13:05:40 -0600

    Add test that checks for expected OSCAL schema version

--- internal/oscal/generate_test.go
+++ internal/oscal/generate_test.go
@@ -1,8 +1,45 @@
 package oscal
 
 import (
+	"encoding/json"
+	"os"
+	"strings"
 	"testing"
 )
+
+var oscalJSONSchemaFile string = "../../test/oscal_component_schema.json"
+
+// Test that the OSCAL schema version is correct
+func TestOscalSchemaVersion(t *testing.T) {
+	oscalMap := make(map[string]interface{})
+
+	oscalBytes, err := os.ReadFile(oscalJSONSchemaFile)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = json.Unmarshal(oscalBytes, &oscalMap)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Check whether the top-level "$id" field is empty or not
+	// if it is, fail the test and print error message
+	// if it isn't, validate the string contains the proper version number
+	if schemaVersion := oscalMap["$id"].(string); schemaVersion != "" {
+		schemaVersion = oscalMap["$id"].(string)
+
+		expected := true
+		actual := strings.Contains(schemaVersion, "1.0.4")
+
+		if expected != actual {
+			t.Error("OSCAL JSON schema version 1.0.4 was not found. The version may have been updated.")
+		}
+	} else {
+		t.Error("The top-level '$id' field was not found or is not populated. Please verify that the OSCAL JSON schema file is valid.")
+	}
+
+}
 
 // TestFmtFieldName tests that we handle formatting a json string to a go struct correctly
 func TestFmtFieldName(t *testing.T) {
