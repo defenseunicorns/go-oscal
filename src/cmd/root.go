12ff605d832557bc60dfd598eed27593d66e9d4e
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2023-02-22 17:49:14 -0600

    Split run function up to be more modular

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -23,7 +23,7 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-oscal",
-	Args:  cobra.MaximumNArgs(0),
+	Args:  cobra.ExactArgs(0),
 	Short: "Generate Go data types from OSCAL JSON schema",
 	Long:  "Generate Go data types from OSCAL JSON schema",
 	Run: func(cmd *cobra.Command, args []string) {
@@ -49,20 +49,12 @@ func init() {
 }
 
 func run() {
-	if format != "json" && format != "yaml" {
-		fmt.Fprintln(os.Stderr, "fmt must be json or yaml")
-		os.Exit(1)
-	}
+	checkForEmptyInput()
+	checkInputDataFormat()
+	tagList := formatTags()
 
-	tagList := make([]string, 0)
-	if tags == "" || tags == format {
-		tagList = append(tagList, format)
-	} else {
-		tagList = strings.Split(tags, ",")
-	}
-
-	var input io.Reader
-	input = os.Stdin
+	// Read provided input.
+	var input = os.Stdin
 	if inputFileName != "" {
 		f, err := os.Open(inputFileName)
 		if err != nil {
@@ -72,8 +64,53 @@ func run() {
 		input = f
 	}
 
-	var convertFloats bool
-	var parser oscal.Parser
+	// Parse the input.
+	parser, convertFloats := parseInput()
+
+	// Generate the Go structs.
+	output := generateStructs(input, parser, pkg, tagList, convertFloats)
+
+	// Write the Go struct output to either stdout or a file.
+	writeOutput(output)
+}
+
+// checkforEmptyInput checks whether the user provided any input or not.
+func checkForEmptyInput() {
+	// If there was no input provided, exit and notify the user to provide input.
+	stats, err := os.Stdin.Stat()
+	if err != nil {
+		log.Fatalf("reading STDIN: %s", err)
+	}
+	if stats.Size() == 0 && inputFileName == "" {
+		fmt.Println("You must provide input either via STDIN or the --input-file flag.")
+		fmt.Println("Use the --help flag to see available flags.")
+		os.Exit(1)
+	}
+}
+
+// checkInputDataFormat checks if the input data format is json or yaml.
+// If it isn't, it exits the program.
+func checkInputDataFormat() {
+	if format != "json" && format != "yaml" {
+		fmt.Fprintln(os.Stderr, "fmt must be json or yaml")
+		os.Exit(1)
+	}
+}
+
+// formatTags formats Go struct tags.
+func formatTags() (tagList []string) {
+	if tags == "" || tags == format {
+		tagList = append(tagList, format)
+	} else {
+		tagList = strings.Split(tags, ",")
+	}
+
+	return
+}
+
+// parseInput determines which parsing method we will use
+// based on the format of the input data.
+func parseInput() (parser oscal.Parser, convertFloats bool) {
 	switch format {
 	case "json":
 		parser = oscal.ParseJson
@@ -82,12 +119,23 @@ func run() {
 		parser = oscal.ParseYaml
 	}
 
+	return
+}
+
+// generateStructs reads input and generates Go structs from a OSCAL JSON schema file.
+func generateStructs(input io.Reader, parser oscal.Parser, pkg string, tagList []string, convertFloats bool) (output []byte) {
 	output, err := oscal.Generate(input, parser, pkg, tagList, convertFloats)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error parsing", err)
 		os.Exit(1)
 	}
 
+	return
+}
+
+// writeOutput writes the generated Go structs to either a file if provided,
+// or stdout by default.
+func writeOutput(output []byte) {
 	if outputFileName != "" {
 		err := os.WriteFile(outputFileName, output, 0644)
 		if err != nil {
@@ -96,5 +144,4 @@ func run() {
 	} else {
 		fmt.Print(string(output))
 	}
-
 }
