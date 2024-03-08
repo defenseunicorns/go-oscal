51305f8f69aa7f9c2819f58a92b1a23b5a89599a
mike-winberry <mike@defenseunicorns.com>
2024-03-08 08:52:15 -0800

    #160. Refactor ValidateCommand to return a ValidationResponse struct that has the validator, result, and warnings.

--- src/cmd/validate/validate.go
+++ src/cmd/validate/validate.go
@@ -12,6 +12,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type ValidationResponse struct {
+	Validator validation.Validator
+	Result    validation.ValidationResult
+	Warnings  []string
+}
+
 var inputfile string
 var validationResultFile string
 
@@ -20,64 +26,78 @@ var ValidateCmd = &cobra.Command{
 	Short: "validate an oscal document",
 	Long:  "Validate an OSCAL document against the OSCAL schema version specified in the document.",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		validator, validationErr := ValidateCommand(inputfile)
+		validationResponse, validationErr := ValidateCommand(inputfile)
 
 		// Write validation result if it was specified and exists before returning ValidateCommand error
-		validationResult, err := validator.GetValidationResult()
-		if err == nil && validationResultFile != "" {
-			err = validation.WriteValidationResult(validationResult, validationResultFile)
+		if validationResultFile != "" {
+			err := validation.WriteValidationResult(validationResponse.Result, validationResultFile)
 			if err != nil {
 				log.Printf("Failed to write validation result to %s: %s\n", validationResultFile, err)
 			}
 		}
-
 		// Return the error from the validation if there was one
 		if validationErr != nil {
 			return validationErr
 		}
 
+		if len(validationResponse.Warnings) > 0 {
+			for _, warning := range validationResponse.Warnings {
+				log.Print(warning)
+			}
+		}
+
 		// No errors, log success
-		log.Printf("Successfully validated %s is valid OSCAL version %s %s\n", inputfile, validator.GetSchemaVersion(), validator.GetModelType())
+		log.Printf("Successfully validated %s is valid OSCAL version %s %s\n", inputfile, validationResponse.Validator.GetSchemaVersion(), validationResponse.Validator.GetModelType())
 		return nil
 	},
 }
 
-func ValidateCommand(inputFile string) (validator validation.Validator, err error) {
+func ValidateCommand(inputFile string) (validationResponse ValidationResponse, err error) {
 	// Validate the input file
 	if inputFile == "" {
-		return validator, errors.New("please specify an input file with the -f flag")
+		return validationResponse, errors.New("please specify an input file with the -f flag")
 	}
 
 	// Validate the input file is a json or yaml file
 	if !strings.HasSuffix(inputFile, "json") && !strings.HasSuffix(inputFile, "yaml") {
-		return validator, errors.New("please specify a json or yaml file")
+		return validationResponse, errors.New("please specify a json or yaml file")
 	}
 
 	// Read the input file
 	bytes, err := os.ReadFile(inputFile)
 	if err != nil {
-		return validator, fmt.Errorf("reading input file: %s", err)
+		return validationResponse, fmt.Errorf("reading input file: %s", err)
 	}
 
-	validator, err = validation.NewValidator(bytes)
+	validator, err := validation.NewValidator(bytes)
 	if err != nil {
-		return validator, fmt.Errorf("failed to create validator: %s", err)
+		return validationResponse, fmt.Errorf("failed to create validator: %s", err)
 	}
 
 	version := validator.GetSchemaVersion()
 	err = utils.VersionWarning(version)
 	if err != nil {
-		log.Print(err)
+		validationResponse.Warnings = append(validationResponse.Warnings, err.Error())
 	}
 
 	validator.SetDocumentPath(inputFile)
 
-	err = validator.Validate()
-	if err != nil {
-		return validator, fmt.Errorf("failed to validate %s version %s: %s", validator.GetModelType(), validator.GetSchemaVersion(), err)
+	validationError := validator.Validate()
+
+	// Write validation result if it was specified and exists before returning ValidateCommand error
+	validationResult, _ := validator.GetValidationResult()
+
+	validationResponse = ValidationResponse{
+		Validator: validator,
+		Result:    validationResult,
+		Warnings:  validationResponse.Warnings,
 	}
 
-	return validator, nil
+	if validationError != nil {
+		return validationResponse, fmt.Errorf("failed to validate %s version %s: %s", validator.GetModelType(), validator.GetSchemaVersion(), err)
+	}
+
+	return validationResponse, nil
 }
 
 func init() {
