75039fed479a2ddca31a52916c8762b4bfef8352
mike-winberry <mike@defenseunicorns.com>
2024-03-08 09:40:15 -0800

    #160. commenting

--- src/cmd/validate/validate.go
+++ src/cmd/validate/validate.go
@@ -52,6 +52,8 @@ var ValidateCmd = &cobra.Command{
 	},
 }
 
+// ValidateCommand validates an OSCAL document
+// Returns a ValidationResponse and an error
 func ValidateCommand(inputFile string) (validationResponse ValidationResponse, err error) {
 	// Validate the input file
 	if inputFile == "" {
@@ -69,30 +71,31 @@ func ValidateCommand(inputFile string) (validationResponse ValidationResponse, e
 		return validationResponse, fmt.Errorf("reading input file: %s", err)
 	}
 
+	// Create and set the validator in the validation response
 	validator, err := validation.NewValidator(bytes)
 	if err != nil {
 		return validationResponse, fmt.Errorf("failed to create validator: %s", err)
 	}
+	validationResponse.Validator = validator
 
+	// Get and set version warnings
 	version := validator.GetSchemaVersion()
 	err = utils.VersionWarning(version)
 	if err != nil {
 		validationResponse.Warnings = append(validationResponse.Warnings, err.Error())
 	}
 
+	// Set the document path
 	validator.SetDocumentPath(inputFile)
 
+	// Run the validation
 	validationError := validator.Validate()
 
 	// Write validation result if it was specified and exists before returning ValidateCommand error
 	validationResult, _ := validator.GetValidationResult()
+	validationResponse.Result = validationResult
 
-	validationResponse = ValidationResponse{
-		Validator: validator,
-		Result:    validationResult,
-		Warnings:  validationResponse.Warnings,
-	}
-
+	// Handle the validation error
 	if validationError != nil {
 		return validationResponse, fmt.Errorf("failed to validate %s version %s: %s", validator.GetModelType(), validator.GetSchemaVersion(), err)
 	}
