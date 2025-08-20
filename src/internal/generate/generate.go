c2ad0e0b6b9dbcb6fd9648c7b46c3c140a9fd4dd
Brandt Keller <brandt.keller@defenseunicorns.com>
2025-08-20 03:38:33 +0000

    feat(generate): parse version from schema id
    Signed-off-by: Brandt Keller <brandt.keller@defenseunicorns.com>

--- src/internal/generate/generate.go
+++ src/internal/generate/generate.go
@@ -66,6 +66,16 @@ func Generate(oscalSchema []byte, pkgName string, tags []string) (typeBytes []by
 	// Add additional imports
 	typeString += buildImportString()
 
+	// Parse the schema ID for the version reference
+	schemaId := schema.ID
+	if schemaId != nil {
+		version, err := extractVersion(*schemaId)
+		if err != nil {
+			return typeBytes, err
+		}
+		typeString += fmt.Sprintf("const OSCAL_VERSION = %s/n", version)
+	}
+
 	// Add the struct definitions in order of creation.
 	for _, ref := range config.refQueue.History() {
 		typeString += config.structMap[ref] + "\n\n"
